be6aff1fdc248e9656b2dc27d5572e5facf33ef5
Sahidur Suman <suman5040@gmail.com>
2018-05-03 13:39:05 +0600

    Setup rspec and added validation and scope tests for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  scope :active_users, -> { where(active: true) }
+  scope :inactive_users, -> { where(active: false) }
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
